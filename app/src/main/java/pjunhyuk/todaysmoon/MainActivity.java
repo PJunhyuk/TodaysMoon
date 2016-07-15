af05fa5cc8d9a10a1e48a6480a3de5c4577997b2
PJunhyuk <jgravity@naver.com>
2016-07-16 04:07:23 +0900

    Add sample cloud image on main page and Make it animate

--- app/src/main/java/pjunhyuk/todaysmoon/MainActivity.java
+++ app/src/main/java/pjunhyuk/todaysmoon/MainActivity.java
@@ -2,37 +2,24 @@ package pjunhyuk.todaysmoon;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton imgbutton;
+    ImageView cloud_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // For SplashActivity
-        startActivity(new Intent(this, SplashActivity.class));
+//        startActivity(new Intent(this, SplashActivity.class));
 
         // For Setting
         imgbutton = (ImageButton)findViewById(R.id.button_setting);
@@ -44,28 +31,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // For cloud patrol
+        cloud_1 = (ImageView) findViewById(R.id.cloud_1);
+        final Animation animPatrol = AnimationUtils.loadAnimation(this, R.anim.translate);
+        cloud_1.startAnimation(animPatrol);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
